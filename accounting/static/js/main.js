4b42d51388b53600d1218ab5300f96f13a133924
Michael Terrell <cwithmichael@gmail.com>
2018-11-24 11:06:05 -0600

    11 - Prevent input errors with Knockout validation & back-end enforcement
    Validate input with Knockout before attempting to make service call
    Make sure the policy number is numeric
    Make sure the policy number and date are both required
    
    On the back-end make sure the request parameters are valid

--- accounting/static/js/main.js
+++ accounting/static/js/main.js
@@ -1,24 +1,67 @@
+ko.validation.init({
+    registerExtenders: true,
+    messagesOnModified: true,
+    insertMessages: true,
+    parseInputAttributes: true,
+    messageTemplate: null
+}, true);
+
+var isANumber = function(val) {
+    return isNaN(val) == false;
+}
+
 var policyViewModel = {
-    policyNumber : ko.observable(),
-    date : ko.observable(),
+    policyNumber : ko.observable().extend({
+        required: {
+            message: "The policy number is required"
+        },
+        validation: {
+            validator: isANumber,
+            message: "It has to be numeric."
+        }
+    }),
+    date : ko.observable().extend({
+        required: {
+            message: "A valid date is required"
+        } 
+    }),
     accountBalance : ko.observable(),
     invoices : ko.observableArray(),
     executeSearch : function() {
         var pn = policyViewModel.policyNumber();
         var date = policyViewModel.date();
-       
-        $.get(encodeURI("http://localhost:5000/account_balance?policy_id=" + pn +"&date_req=" + date), 
-            function(data) {
-                policyViewModel.accountBalance("Account Balance:" + data.balance);
-            }
-        );
 
-        $.get(encodeURI("http://localhost:5000/invoices?policy_id=" + pn +"&date_req=" + date), 
-            function(data) {
-                policyViewModel.invoices(data.invoices);
+        if (policyViewModel.errors().length !== 0) {
+            alert(policyViewModel.errors()[0]);
+            return;
+        }
+       
+        $.ajax({
+            type: "GET",
+            url: encodeURI("http://localhost:5000/account_balance?policy_id=" + pn +"&date_req=" + date), 
+            error: function(xhr, status) {
+                policyViewModel.accountBalance("");
+                alert ("Error: Policy not found" ); 
+            },
+            success: function(data) {
+                if (data.balance) {
+                    policyViewModel.accountBalance("Account Balance: " + data.balance);
+                }
             }
-        );
+        });
+
+        $.ajax({
+            type: "GET",
+            url: encodeURI("http://localhost:5000/invoices?policy_id=" + pn +"&date_req=" + date), 
+            success: function(data) {
+                if (data.invoices) {
+                    policyViewModel.invoices(data.invoices);
+                }
+            }
+        });
     }
 }
+
+policyViewModel.errors = ko.validation.group(policyViewModel);
 
 ko.applyBindings(policyViewModel);
--- accounting/views.py
+++ accounting/views.py
@@ -9,28 +9,49 @@ from models import Contact, Invoice, Policy
 
 # Import PolicyAccoutning to get account balance 
 from utils import PolicyAccounting
-from datetime import date
+from datetime import date, datetime
+
+import unicodedata
+
+def validReqParams(policy_id, dateReq):
+    """Validate the request parameters."""
+    if not policy_id or not dateReq: return False
+    try:
+        datetime.strptime(dateReq, '%Y-%m-%d') # check if date follows format
+    except ValueError:
+        return False
+    try:
+        float(policy_id) # make sure policy id is actually a number
+    except ValueError:
+        return False
+    return True
 
 # Routing for the server.
 @app.route("/")
 def index():
-    # You will need to serve something up here.
     return render_template('index.html')
 
 @app.route("/invoices")
 def show_invoices():
     policy_id  = request.args.get('policy_id', None)
     dateReq  = request.args.get('date_req', None)
+    if not validReqParams(policy_id, dateReq):
+        return jsonify(error={"message": "invalid params"}), 400
     # return all the invoices for the policy
     invoices = Invoice.query.filter_by(policy_id=policy_id)\
-                                    .filter(Invoice.bill_date <= dateReq)\
-                                    .order_by(Invoice.bill_date)\
-                                    .all()
+                            .filter(Invoice.bill_date <= dateReq)\
+                            .order_by(Invoice.bill_date)\
+                            .all()
     return jsonify(invoices=[i.serialize() for i in invoices])
     
 @app.route("/account_balance")
 def show_account_balance():
     policy_id  = request.args.get('policy_id', None)
     dateReq  = request.args.get('date_req', None)
-    pa = PolicyAccounting(int(policy_id))
-    return jsonify(balance=pa.return_account_balance(dateReq))
+    if not validReqParams(policy_id, dateReq):
+        return jsonify(error={"message": "invalid params" }), 400
+    try:
+        pa = PolicyAccounting(int(policy_id))
+        return jsonify(balance=pa.return_account_balance(dateReq))
+    except:
+        return jsonify(notFound={"message" : "policy not found"}), 404
